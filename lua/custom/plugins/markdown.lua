1759a9dd07e6a1d1a23f86632a03c6e1ba720c6c
ge3224 <ge3224@gmail.com>
2024-05-27 16:12:22 -0500

    Modify custom markdown config

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,11 @@
+return {
+  { 'artempyanykh/marksman' },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
+}
