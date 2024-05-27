5ee2dcdf0a89f5efcbf7769fc5fc1a9ce80a67aa
ge3224 <ge3224@gmail.com>
2024-05-27 17:34:45 -0500

    Modify fugitive config

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -26,8 +26,5 @@ vim.keymap.set('n', '<leader>lr', '<cmd>LspRestart<CR>', { silent = true })
 -- open a small terminal
 vim.keymap.set('n', '<leader>st', '<cmd>new<CR><C-w>J<cmd>resize 16<CR><cmd>term<CR>', { silent = true })
 
--- fugitive mappings
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-
 -- delete cached, i.e. swap file
 vim.keymap.set('n', '<leader>zc', '<cmd>call system("rm -rdf ~/.local/state/nvim/swap")<CR>', { silent = true })
--- lua/custom/plugins/vim_fugitive.lua
+++ lua/custom/plugins/vim_fugitive.lua
@@ -1,3 +1,33 @@
 return {
   'tpope/vim-fugitive',
+  config = function()
+    vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+
+    local fugitive_augroup = vim.api.nvim_create_augroup('fugitive_augroup', {})
+
+    local autocmd = vim.api.nvim_create_autocmd
+    autocmd('BufWinEnter', {
+      group = fugitive_augroup,
+      pattern = '*',
+      callback = function()
+        if vim.bo.ft ~= 'fugitive' then
+          return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = { buffer = bufnr, remap = false }
+        vim.keymap.set('n', '<leader>p', function()
+          vim.cmd.Git 'push'
+        end, opts)
+
+        -- rebase always
+        vim.keymap.set('n', '<leader>P', function()
+          vim.cmd.Git { 'pull', '--rebase' }
+        end, opts)
+      end,
+    })
+
+    vim.keymap.set('n', 'gl', '<cmd>diffget //2<CR>')
+    vim.keymap.set('n', 'gr', '<cmd>diffget //3<CR>')
+  end,
 }
