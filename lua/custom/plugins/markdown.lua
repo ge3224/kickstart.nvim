a1292324edcca3b5538491febba0590ecb591f52
ge3224 <ge3224@gmail.com>
2024-09-07 16:18:31 -0500

    modify markdown config

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,11 +1,4 @@
 return {
   { 'artempyanykh/marksman' },
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function()
-      vim.fn['mkdp#util#install']()
-    end,
-  },
+  { 'ellisonleao/glow.nvim', config = true, cmd = 'Glow' },
 }
