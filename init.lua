39b20a0f6af5a2be0d41aa2b1224f896b1d623f7
ge3224 <ge3224@gmail.com>
2024-11-15 12:11:29 -0600

    feat: use typescript-tools

--- init.lua
+++ init.lua
@@ -1,6 +1,13 @@
 --[[ todo ]]
 
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
