45fa802cba811e3a79eee769fa91774c38aa3d41
ge3224 <ge3224@gmail.com>
2024-11-18 20:36:20 -0600

    feat: adjust outline plugin config

--- lua/custom/plugins/outline.lua
+++ lua/custom/plugins/outline.lua
@@ -1,15 +1,29 @@
 return {
-  'stevearc/aerial.nvim',
-  opts = {},
-  keys = {
-    {
-      '<leader>to',
-      '<cmd>AerialToggle!<CR>',
-      desc = 'Toggle Code Outline',
-    },
-  },
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-tree/nvim-web-devicons',
-  },
+  "stevearc/aerial.nvim",
+  config = function()
+    require("aerial").setup {
+      on_attach = function(bufnr)
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+
+      vim.keymap.set("n", "<leader>to", "<cmd>AerialToggle!<CR>"),
+
+      layout = {
+        min_width = { 20 },
+      },
+    }
+  end,
+  -- opts = {},
+  -- keys = {
+  --   {
+  --     '<leader>to',
+  --     '<cmd>AerialToggle!<CR>',
+  --     desc = 'Toggle Code Outline',
+  --   },
+  -- },
+  -- dependencies = {
+  --   'nvim-treesitter/nvim-treesitter',
+  --   'nvim-tree/nvim-web-devicons',
+  -- },
 }
